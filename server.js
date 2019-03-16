ccd2706ab96d87446f4d3eb1b4bb8ad3bdec1f0d
Jessica Yin <jyin978@hotmail.com>
2019-03-15 21:32:50 -0700

    added new host connection and ssl

--- server.js
+++ server.js
@@ -12,11 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-aerodynamic-36670',
-    user : 'postgres',
-    port: '5432',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true;
   }
 });
 
