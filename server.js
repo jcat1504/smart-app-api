cddc43c6d3229a7e7411775217e9dcee6aa12510
Jessica Yin <jyin978@hotmail.com>
2019-03-20 15:32:43 -0700

    cors again

--- server.js
+++ server.js
@@ -22,17 +22,10 @@ const app = express();
 
 
 app.use(function(req, res, next) {
-  req.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
-app.all('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next()
-  });
 
 
 app.use(bodyParser.json());
